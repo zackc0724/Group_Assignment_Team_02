7dd8541ab143256af7d4065e461066acba87f870
zenish-borad <borad.z@northeastern.edu>
2025-10-26 23:48:06 -0400

    Zenish Faculty Use case
    This is the faculty use case consists all of the functions worked by faculty. It includes Course Management, Student Management, ProfileManagement, Enrollment Insight and Grading

--- Group_Assignment_Team_02_Zihao(Admin)/src/model/Admin.java
+++
@@ -1,8 +0,0 @@
-
-package model;
-
-public class Admin extends Person {
-    public Admin(String universityId, String name, String email, String phone, String department) {
-        super(universityId, name, email, phone, department);
-    }
-}
